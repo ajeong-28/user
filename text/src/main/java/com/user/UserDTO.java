68fc8948f0cde15ee881f95359a24c272e29fc6b
aj <aj@DESKTOP-2OSLSHL>
2022-04-25 15:10:45 +0900

    두번째

--- text/src/main/java/com/user/UserDTO.java
+++ text/src/main/java/com/user/UserDTO.java
@@ -2,6 +2,15 @@ package com.user;
 
 public class UserDTO {
 	private String userId;
+	private String userName;
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userNmae) {
+		this.userName = userNmae;
+	}
 
 	public String getUserId() {
 		return userId;
